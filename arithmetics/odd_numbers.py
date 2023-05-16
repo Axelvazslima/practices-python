2c10457d1dea472eab2286ffcca2d62879412697
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2023-05-16 15:37:50 -0300

    Function that returns a list of all odd nums in a certain range

---
+++ arithmetics/odd_numbers.py
@@ -0,0 +1,19 @@
+def odd_numbers():
+    print("You will input a number (1 <= int <= 1000) and the programm will return you a list of all odd numbers before or equal that")
+    x = int(input("Input a number: "))
+
+    cond = x in range(1, 1001)
+    over = x > 1000
+    under = x < 1
+    msg = f"Your number doesn't match the conditions for the code to run. You picked {x}."
+    if not cond and over:
+        return f"{msg} {x} > 1000."
+    elif not cond and under:
+        return f"{msg} {x} < 1."
+
+    odds = []
+    for i in range(x + 1):
+        odd = i % 2 != 0
+        if odd:
+            odds.append(i)
+    return odds
