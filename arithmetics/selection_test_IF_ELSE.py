c0617d05402c11fd44b917f0eea2543b19cc6ee6
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2023-05-21 00:45:16 -0300

    :build: changed return of function

--- arithmetics/selection_test_IF_ELSE.py
+++ arithmetics/selection_test_IF_ELSE.py
@@ -2,8 +2,6 @@ def selection():
     A, B, C, D = map(int, input().split())
 
     condition = B > C and D > A and (C + D) > (A + B) and C >= 0 and D >= 0 and A % 2 == 0
+    
+    return condition
 
-    if condition:
-        print("Valores aceitos")
-    else:
-        print("Valores nao aceitos")
