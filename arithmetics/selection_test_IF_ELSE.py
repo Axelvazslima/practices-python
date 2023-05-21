ef5d302f0a4f261364cab0b7f0ab57aedaad7529
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2023-05-21 00:32:36 -0300

    Logical test using IF/ELSE and some conditions

---
+++ arithmetics/selection_test_IF_ELSE.py
@@ -0,0 +1,9 @@
+def selection():
+    A, B, C, D = map(int, input().split())
+
+    condition = B > C and D > A and (C + D) > (A + B) and C >= 0 and D >= 0 and A % 2 == 0
+
+    if condition:
+        print("Valores aceitos")
+    else:
+        print("Valores nao aceitos")
