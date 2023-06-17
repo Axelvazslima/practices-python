3b409c387509f779db97cc6338c611ab22ca7681
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2023-06-17 14:47:49 -0300

    :bug:

--- data_structures/reversing_lists.py
+++ data_structures/reversing_lists.py
@@ -1,16 +1,19 @@
 def reversing_lists(wanted_list):
     sort_question = int(input("""\nThis function receives a list as an argument and returns it but reversed.
-It uses the Two pointer method, which makes it more efficient and faster.
-* it's up to you, but the list should be sorted.
+It uses the bubblesort algorithm.
+Do you want it reversed, sorted in decrescent order or sorted in crescent order?
 
-Do you want it sorted? >> 0 for 'no', 1 for 'yes': """))
-    if sort_question:
-        wanted_list.sort()
-    i = 0
-    j = len(wanted_list)-1
-    print(i, j)
-    while i < j:
-        wanted_list[i], wanted_list[j] = wanted_list[j], wanted_list[i]
-        i += 1
-        j -= 1
+Type, respectively; 1, 2 or 3: """))
+    
+    match sort_question:
+        case 1:
+            wanted_list = wanted_list[::-1]
+        case 2:
+            wanted_list.sort()
+            wanted_list = wanted_list[::-1]
+        case 3:
+            wanted_list.sort()
     return wanted_list
+
+
+print(reversing_lists([1, 4, 6, 8, 7]))
